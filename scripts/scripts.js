8d5b91d04557955c67be73d28b761ba3728d8a1d
Christopher Stephen <cstephen82@protonmail.com>
2022-06-10 13:40:54 +1000

    Crypto program

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+function addTwo(int1, int2) {
+  return int1 + int2;
+}
+
+function outputText() {
+  var text = document.getElementById("txta1").value;
+  document.getElementById("p3").innerHTML = text;
+}
